43c4ab101cb6004b43ac5fda8339e04559bc949f
pnhung177 <pnhung177@acegik.net>
2017-07-21 17:25:30 +0700

    Add destroy() method to master, worker; Add TDD for one master - multiple workers case

--- test/tdd/master-worker-test.js
+++ test/tdd/master-worker-test.js
@@ -12,6 +12,9 @@ var appCfg = require('../lab/app-configuration');
 var bogen = require('../lab/big-object-generator');
 var Loadsync = require('loadsync');
 
+var Fibonacci = require('../lab/fibonacci').Fibonacci;
+var fibonacci = require('../lab/fibonacci').fibonacci;
+
 describe('opflow-master:', function() {
 
 	describe('one master - one worker:', function() {
@@ -34,7 +37,10 @@ describe('opflow-master:', function() {
 		});
 
 		afterEach(function(done) {
-			done();
+			Promise.all([
+				master.destroy(),
+				worker.destroy()
+			]).then(lodash.ary(done, 0));
 		});
 
 		it('master request, worker process and response', function(done) {
@@ -84,29 +90,94 @@ describe('opflow-master:', function() {
 			});
 		});
 	});
+
+	describe('one master - multiple workers:', function() {
+		var master, worker1, worker2;
+
+		var taskWorker = function(data, done, notifier) {
+			debugx.enabled && debugx('Input data: %s', data);
+			var fibonacci = new Fibonacci(JSON.parse(data));
+			while(fibonacci.next()) {
+				var r = fibonacci.result();
+				notifier.progress(r.step, r.number);
+			};
+			done(null, fibonacci.result());
+		};
+
+		before(function() {
+			var cfg = appCfg.extend({
+				feedback: {
+					queueName: 'tdd-opflow-feedback',
+					durable: true,
+					noAck: false
+				}
+			});
+			master = new opflow.Master(cfg);
+			worker1 = new opflow.Worker(cfg);
+			worker2 = new opflow.Worker(cfg);
+		});
+
+		beforeEach(function(done) {
+			done();
+		});
+
+		afterEach(function(done) {
+			Promise.all([
+				master.destroy(),
+				worker1.destroy(),
+				worker2.destroy()
+			]).then(lodash.ary(done, 0));
+		});
+
+		it('master request to multiple workers, it should return correct results', function(done) {
+			this.timeout(100000);
+			worker1.process(taskWorker);
+			worker2.process(taskWorker);
+			var data = [10, 8, 20, 15, 11, 19, 25, 12, 16, 25, 34, 28].map(function(n) { return { number: n }});
+			var promises = Promise.map(data, function(input) {
+				return master.execute(input).then(function(job) {
+					var requestID = job.requestId;
+					return new Promise(function(onResolved, onRejected) {
+						var stepTracer = [];
+						job.on('started', function(info) {
+							stepTracer.push({ event: 'started', data: info});
+							debugx.enabled && debugx('Task[%s] started', requestID);
+						}).on('progress', function(percent, data) {
+							stepTracer.push({ event: 'progress', data: {percent: percent}});
+							debugx.enabled && debugx('Task[%s] progress: %s', requestID, percent);
+						}).on('failed', function(error) {
+							stepTracer.push({ event: 'failed', data: error});
+							debugx.enabled && debugx('Task[%s] failed, error: %s', requestID, JSON.stringify(error));
+							onRejected(error);
+						}).on('completed', function(result) {
+							stepTracer.push({ event: 'completed', data: result});
+							debugx.enabled && debugx('Task[%s] done, result: %s', requestID, JSON.stringify(result));
+							setTimeout(function() {
+								onResolved(stepTracer)
+							}, lodash.random(100));
+						});
+					});
+				}).then(function(trail) {
+					return { input: input, trail: trail }
+				});
+			}, {concurrency: 4});
+			Promise.all(promises).then(function(results) {
+				lodash.forEach(results, function(result) {
+					var input = result.input;
+					var trail = result.trail;
+					assert.equal(trail.length, 1 + input.number + 1);
+					assert.equal(trail[0].event, 'started');
+					for(var i=1; i<=input.number; i++) {
+						assert.equal(trail[i].event, 'progress');
+					}
+					assert.equal(trail[input.number + 1].event, 'completed');
+					assert.equal(trail[input.number + 1].data.number, input.number);
+					assert.equal(trail[input.number + 1].data.value, fibonacci(input.number));
+				});
+				done(null);
+			}).catch(function(error) {
+				done(error);
+			});
+		});
+	});
 });
-
-var Fibonacci = function Fibonacci(P) {
-	var n = P && P.number && P.number >= 0 ? P.number : null;
-	var c = 0;
-	var f = 0, f_1 = 0, f_2 = 0;
-
-	this.next = function() {
-		if (c >= n) return false;
-		if (++c < 2) {
-			f = c;
-		} else {
-			f_2 = f_1; f_1 = f; f = f_1 + f_2;
-		}
-		return true;
-	}
-
-	this.result = function() {
-		return { value: f, step: c, number: n };
-	}
-}
-
-var fibonacci = function fibonacci(n) {
-	if (n == 0 || n == 1) return n;
-	else return fibonacci(n - 1) + fibonacci(n - 2);
-}
