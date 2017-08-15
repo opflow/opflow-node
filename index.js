56f1a8492f6bf442f5e11beb0303437b13203477
pnhung177 <pnhung177@acegik.net>
2017-08-15 13:59:34 +0700

    Split RpcMaster and RpcWorker

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
 	Engine: require('./lib/engine'),
-	Master: require('./lib/master'),
-	Worker: require('./lib/worker'),
 	Pubsub: require('./lib/pubsub'),
-	Runner: require('./lib/runner')
+	Master: require('./lib/master'),
+	Worker: require('./lib/worker')
 }
