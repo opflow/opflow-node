5559194e60eef9d86fb5c96dc2f22a09bffead34
pnhung177 <pnhung177@acegik.net>
2017-09-02 21:41:19 +0700

    Clean source code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
 	Engine: require('./lib/engine'),
 	Executor: require('./lib/executor'),
 	PubsubHandler: require('./lib/pubsub'),
+	Recycler: require('./lib/recycler'),
 	RpcMaster: require('./lib/rpc_master'),
 	RpcWorker: require('./lib/rpc_worker')
 }
---
+++ test/cmd/bdd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+node_modules/.bin/mocha --recursive test/bdd/**/*-test.js;
+true
