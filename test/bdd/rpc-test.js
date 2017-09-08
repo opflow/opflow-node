038c11c07e8a434496e217d04889b9f3e349efa9
pnhung177 <pnhung177@acegik.net>
2017-09-08 09:14:49 +0700

    RPC skip unmanaged exception

--- test/bdd/rpc-test.js
+++ test/bdd/rpc-test.js
@@ -15,7 +15,7 @@ var fibonacci = require('../lab/fibonacci').fibonacci;
 
 describe('opflow-master:', function() {
 
-	describe('one master - one worker:', function() {
+	describe('single master - single worker:', function() {
 		var master, worker;
 
 		before(function() {
@@ -63,7 +63,7 @@ describe('opflow-master:', function() {
 		});
 	});
 
-	describe('one master - multiple workers:', function() {
+	describe('single master - multiple workers:', function() {
 		var master, worker1, worker2;
 
 		before(function() {
@@ -93,9 +93,9 @@ describe('opflow-master:', function() {
 
 		afterEach(function(done) {
 			Promise.all([
+				master.close(),
 				worker1.close(),
-				worker2.close(),
-				master.close()
+				worker2.close()
 			]).then(lodash.ary(done, 0));
 		});
 
@@ -116,6 +116,86 @@ describe('opflow-master:', function() {
 			}).then(function(results) {
 				lodash.forEach(results, validateResult);
 				done(null);
+			}).catch(function(error) {
+				done(error);
+			});
+		});
+	});
+
+	describe('bypass unmanaged exception:', function() {
+		var master, worker1, worker2;
+
+		before(function() {
+			master = new opflow.RpcMaster(appCfg.extend({
+				routingKey: 'tdd-opflow-rpc',
+				responseName: 'tdd-opflow-response',
+				monitorTimeout: 5000,
+				autoinit: false
+			}));
+			var cfg = appCfg.extend({
+				routingKey: 'tdd-opflow-rpc',
+				responseName: 'tdd-opflow-response',
+				operatorName: 'tdd-opflow-operator',
+				autoinit: false
+			});
+			worker1 = new opflow.RpcWorker(cfg);
+			worker2 = new opflow.RpcWorker(cfg);
+		});
+
+		beforeEach(function(done) {
+			Promise.all([
+				master.ready(),
+				worker1.ready(),
+				worker2.ready()
+			]).then(lodash.ary(done, 0));
+		});
+
+		afterEach(function(done) {
+			Promise.all([
+				master.close(),
+				worker1.close(),
+				worker2.close()
+			]).then(lodash.ary(done, 0));
+		});
+
+		it('should bypass unmanged exception, workers are still alive', function(done) {
+			this.timeout(120000);
+			var data = [10, 8, 20, 15, 11, 19, 60, 25, 12, 77, 16, 35, 50, 34, 28].map(function(n) { return { number: n }});
+			var taskRejectValues = function(body, headers, response) {
+				debugx.enabled && debugx('Request[%s] receives: %s', headers.requestId, body);
+				body = JSON.parse(body);
+				response.emitStarted();
+				if (body.number == 60) throw new Error('failed with: ' + body.number);
+				var fibonacci = new Fibonacci(body);
+				while(fibonacci.next()) {
+					var r = fibonacci.result();
+					response.emitProgress(r.step, r.number);
+					if (body.number == 77 && r.step > 40) {
+						throw new Error('failed with: ' + body.number);
+					}
+				};
+				if (body.number == 50) throw new Error('failed with: ' + body.number);
+				response.emitCompleted(fibonacci.result());
+			}
+			Promise.all([
+				worker1.process('fibonacci', taskRejectValues),
+				worker2.process('fibonacci', taskRejectValues)
+			]).then(function() {
+				return Promise.map(data, function(input) {
+					return master.request('fibonacci', input).then(function(job) {
+						return job.extractResult();
+					});
+				}, {concurrency: 4});
+			}).then(function(results) {
+				assert.equal(results.length, data.length);
+				Promise.reduce(results, function(acc, result) {
+					if (result.completed) acc.success += 1;
+					return acc;
+				}, { success: 0 }).then(function(stats) {
+					assert.equal(stats.success, data.length - 3);
+					debugx.enabled && debugx('Success total: %s', stats.success);
+					done(null);
+				});
 			}).catch(function(error) {
 				done(error);
 			});
