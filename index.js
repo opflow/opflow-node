c6ac7609f925cfd227519d1064c00faf89c612bf
pnhung177 <pnhung177@acegik.net>
2017-09-20 17:25:34 +0700

    Release version 0.1.5

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = {
 	Engine: require('./lib/engine'),
 	Executor: require('./lib/executor'),
 	Loader: require('./lib/loader'),
+	LogAdapter: require('./lib/log_adapter'),
+	LogTracer: require('./lib/log_tracer'),
 	PubsubHandler: require('./lib/pubsub'),
 	Recycler: require('./lib/recycler'),
 	RpcMaster: require('./lib/rpc_master'),
