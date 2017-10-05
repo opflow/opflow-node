48067bb0b719e989483ef9efc9f414a14e2c92c8
pnhung177 <pnhung177@acegik.net>
2017-10-05 15:14:02 +0700

    build URI from connection parameters

--- test/tdd/util-test.js
+++ test/tdd/util-test.js
@@ -7,3 +7,29 @@ var expect = require('chai').expect;
 var debugx = require('debug')('tdd:opflow:util');
 var misc = require('../../lib/util');
 var LogAdapter = require('../../lib/log_adapter');
+
+describe('opflow.util:', function() {
+	it('should build default URI from empty parameters', function() {
+		var args = misc.buildURI();
+		assert.equal(args.uri, 'amqp://localhost');
+		assert.equal(args.host, 'localhost');
+		debugx.enabled && debugx('conargs (default) %s', JSON.stringify(args));
+	});
+
+	it('should build full URI from completed parameters', function() {
+		var args = misc.buildURI({
+			host: 'broker.example.com',
+			port: 5670,
+			username: 'nowhere',
+			password: 'happy',
+			virtualHost: 'opflow',
+			channelMax: 10,
+			frameMax: '0x1000',
+			heartbeat: 30,
+			locale: 'vi_VN'
+		});
+		assert.equal(args.uri, "amqp://nowhere:happy@broker.example.com:5670" +
+			"/opflow?channelMax=10&frameMax=0x1000&heartbeat=30&locale=vi_VN");
+		debugx.enabled && debugx('conargs (full) %s', JSON.stringify(args));
+	});
+});
